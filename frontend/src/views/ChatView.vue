b83cebb68190d442e33c806554c8298d2b17ed6e
Montags24 <adrianmontagu@hotmail.co.uk>
2024-03-18 12:35:37 +0000

    setup frontend with tailwind css

---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <!-- Chat app header -->
+    </section>
+    <section>
+        <!-- Chat app main body section -->
+    </section>
+    <section>
+        <!-- Chat app messages sectuib -->
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
